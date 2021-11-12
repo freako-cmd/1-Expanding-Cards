9465468782c25a3705ba3709578c6a5599e315f8
Cem <ulrich_345@web.de>
2021-11-12 20:53:10 +0100

    add function removeAllActiveClasses

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 
-// querySelectorAll puts every 'class .panel' in a node list and that is similar to an array
+// querySelectorAll puts every class '.panel' in a node list and that is similar to an array
 const panels = document.querySelectorAll('.panel');
 
-// loop through panel and 'forEach()' method calls a function for each element in the array
+// 'forEach' method loops through the variable 'const panels' that is a node list. 
+// 'forEach()' method calls a function for each element in the array (callback function = function that executes on each element of array)
 panels.forEach((panel) => {
+    
     // take each panel and addEventListener and listen for a 'click' and when that happens we run a function
+    // arrow function () => {...}
     panel.addEventListener('click', () => {
-       
         
-        // panel.classList gives a list of the classes snd method of .add - adds one or more class name to an element
+        // will call a function (defined below) to remove the 'class of active' on all of the panels/photos 
+        // that will make sure that if the panel/photo was expanded it now gets smaller again
+        // when we click, the function is going to remove all the active classes from all of the panels/photos 
+        // and then put the active class on the one we click. 
+        removeAllActiveClasses()
+
+        // 'panel.classList' gives a list of the classes/returns the class name of an element 
+        // '.add' method - adds one or more class name(s) to an element
         // by clicking on one 'class panel/photo/card' the clicked one gets added to the class of 'active' and changes to 'css flex: 5;'
         // the clicked panel/photo/card is expanding but not getting smaller
         panel.classList.add('active')
     })
 })
+
+// creates a function that will remove 'class of active' on all panels/photos
+function removeAllActiveClasses() {
+    
+    // 'panels.forEach' loops through 'const = panels' and calls function on each element in the variable 
+    panels.forEach(panel => {
+    
+        // removes all the 'active' classes on all elements in the classList object
+        panel.classList.remove('active')
+    })
+}
